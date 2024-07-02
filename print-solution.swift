9dc2bb6a9dd98e7d40239df9f5a0ece6b73558c6
Jacob Sock <42015295+jacobsock@users.noreply.github.com>
2024-07-02 04:43:04 -0400

    Create print-solution.swift
    add initial print solution

---
+++ print-solution.swift
@@ -0,0 +1,21 @@
+//My name is Jacob Sock. 
+// I like coding and playing the piano.
+//Here is my print solution:
+
+print("JJJJJ")
+print("  J  ")
+print("  J  ")
+print("  J  ")
+print("J J  ")
+print("J J  ")
+print(" JJ")
+
+print("\n")
+
+print(" SSS  ")
+print("S   S")
+print("S   ")
+print(" SSS  ")
+print("   S")
+print("S   S")
+print(" SSS  ")
